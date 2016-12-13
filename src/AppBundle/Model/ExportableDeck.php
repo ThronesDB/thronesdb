2dd9ada12ac48f3dbdf63ffb2fd223dca9e1e98f
Alsciende <bertolini.cedric@me.com>
2016-12-13 16:42:35 +0100

    deprecating agenda_code in api/decklist and adding agendas

--- src/AppBundle/Model/ExportableDeck.php
+++ src/AppBundle/Model/ExportableDeck.php
@@ -8,6 +8,11 @@ class ExportableDeck
     public function getArrayExport ($withUnsavedChanges = false)
     {
         $slots = $this->getSlots();
+        $agendas = $slots->getAgendas();
+        $agendas_code = [];
+        foreach($agendas as $agenda) {
+            $agendas_code[] = $agenda->getCard()->getCode();
+        }
         $array = [
             'id' => $this->getId(),
             'name' => $this->getName(),
@@ -18,7 +23,9 @@ class ExportableDeck
             'faction_code' => $this->getFaction()->getCode(),
             'faction_name' => $this->getFaction()->getName(),
             'slots' => $slots->getContent(),
-            'agendas' => $slots->getAgendas(),
+            'agendas' => $agendas_code,
+            // agenda_code deprecated. Removed 2017-03-01
+            'agenda_code' => count($agendas) ? $agendas[0]->getCard()->getCode() : null,
             'version' => $this->getVersion(),
         ];
 
