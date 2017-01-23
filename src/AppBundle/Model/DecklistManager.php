be328c94ebd5b071933629ab5ef4fa285e479a64
Alsciende <bertolini.cedric@me.com>
2017-01-23 11:26:22 +0100

    fix decklist search when packs not selected

--- src/AppBundle/Model/DecklistManager.php
+++ src/AppBundle/Model/DecklistManager.php
@@ -21,289 +21,292 @@ use Doctrine\Common\Collections\ArrayCollection;
  */
 class DecklistManager
 {
-	protected $faction;
-	protected $page = 1;
-	protected $start = 0;
-	protected $limit = 30;
-	protected $maxcount = 0;
 
-	public function __construct(EntityManager $doctrine, RequestStack $request_stack, Router $router, LoggerInterface $logger)
-	{
-		$this->doctrine = $doctrine;
-		$this->request_stack = $request_stack;
-		$this->router = $router;
-		$this->logger = $logger;
-	}
+    protected $faction;
+    protected $page = 1;
+    protected $start = 0;
+    protected $limit = 30;
+    protected $maxcount = 0;
 
-	public function setFaction(Faction $faction = null)
-	{
-		$this->faction = $faction;
-	}
+    public function __construct (EntityManager $doctrine, RequestStack $request_stack, Router $router, LoggerInterface $logger)
+    {
+        $this->doctrine = $doctrine;
+        $this->request_stack = $request_stack;
+        $this->router = $router;
+        $this->logger = $logger;
+    }
 
-	public function setLimit($limit)
-	{
-		$this->limit = $limit;
-	}
+    public function setFaction (Faction $faction = null)
+    {
+        $this->faction = $faction;
+    }
 
-	public function setPage($page)
-	{
-		$this->page = max($page, 1);
-		$this->start = ($this->page - 1) * $this->limit;
-	}
+    public function setLimit ($limit)
+    {
+        $this->limit = $limit;
+    }
 
-	public function getMaxCount()
-	{
-		return $this->maxcount;
-	}
+    public function setPage ($page)
+    {
+        $this->page = max($page, 1);
+        $this->start = ($this->page - 1) * $this->limit;
+    }
 
-	/**
-	 * creates the basic query builder and initializes it
-	 */
-	private function getQueryBuilder()
-	{
-		$qb = $this->doctrine->createQueryBuilder();
-		$qb->select('d');
-		$qb->from('AppBundle:Decklist', 'd');
-		if($this->faction) {
-			$qb->where('d.faction = :faction');
-			$qb->setParameter('faction', $this->faction);
-		}
-		$qb->setFirstResult($this->start);
-		$qb->setMaxResults($this->limit);
-		$qb->distinct();
-		return $qb;
-	}
+    public function getMaxCount ()
+    {
+        return $this->maxcount;
+    }
 
-	/**
-	 * creates the paginator around the query
-	 * @param Query $query
-	 */
-	private function getPaginator(Query $query)
-	{
-		$paginator = new Paginator($query, $fetchJoinCollection = FALSE);
-		$this->maxcount = $paginator->count();
-		return $paginator;
-	}
+    /**
+     * creates the basic query builder and initializes it
+     */
+    private function getQueryBuilder ()
+    {
+        $qb = $this->doctrine->createQueryBuilder();
+        $qb->select('d');
+        $qb->from('AppBundle:Decklist', 'd');
+        if($this->faction) {
+            $qb->where('d.faction = :faction');
+            $qb->setParameter('faction', $this->faction);
+        }
+        $qb->setFirstResult($this->start);
+        $qb->setMaxResults($this->limit);
+        $qb->distinct();
+        return $qb;
+    }
 
-	public function getEmptyList()
-	{
-		$this->maxcount = 0;
-		return new ArrayCollection([]);
-	}
+    /**
+     * creates the paginator around the query
+     * @param Query $query
+     */
+    private function getPaginator (Query $query)
+    {
+        $paginator = new Paginator($query, $fetchJoinCollection = FALSE);
+        $this->maxcount = $paginator->count();
+        return $paginator;
+    }
 
-	public function findDecklistsByPopularity()
-	{
-		$qb = $this->getQueryBuilder();
-		$qb->addSelect('(1+d.nbVotes)/(1+POWER(DATE_DIFF(CURRENT_TIMESTAMP(), d.dateCreation), 2)) AS HIDDEN popularity');
-		$qb->orderBy('popularity', 'DESC');
-		return $this->getPaginator($qb->getQuery());
-	}
+    public function getEmptyList ()
+    {
+        $this->maxcount = 0;
+        return new ArrayCollection([]);
+    }
 
-	public function findDecklistsByAge($ignoreEmptyDescriptions = FALSE)
-	{
-		$qb = $this->getQueryBuilder();
-		$qb->orderBy('d.dateCreation', 'DESC');
-		return $this->getPaginator($qb->getQuery());
-	}
+    public function findDecklistsByPopularity ()
+    {
+        $qb = $this->getQueryBuilder();
+        $qb->addSelect('(1+d.nbVotes)/(1+POWER(DATE_DIFF(CURRENT_TIMESTAMP(), d.dateCreation), 2)) AS HIDDEN popularity');
+        $qb->orderBy('popularity', 'DESC');
+        return $this->getPaginator($qb->getQuery());
+    }
 
-	public function findDecklistsByFavorite(User $user)
-	{
-		$qb = $this->getQueryBuilder();
-		$qb->leftJoin('d.favorites', 'u');
-		$qb->andWhere('u = :user');
-		$qb->setParameter('user', $user);
-		$qb->orderBy('d.dateCreation', 'DESC');
-		return $this->getPaginator($qb->getQuery());
-	}
+    public function findDecklistsByAge ($ignoreEmptyDescriptions = FALSE)
+    {
+        $qb = $this->getQueryBuilder();
+        $qb->orderBy('d.dateCreation', 'DESC');
+        return $this->getPaginator($qb->getQuery());
+    }
 
-	public function findDecklistsByAuthor(User $user)
-	{
-		$qb = $this->getQueryBuilder();
-		$qb->andWhere('d.user = :user');
-		$qb->setParameter('user', $user);
-		$qb->orderBy('d.dateCreation', 'DESC');
-		return $this->getPaginator($qb->getQuery());
-	}
+    public function findDecklistsByFavorite (User $user)
+    {
+        $qb = $this->getQueryBuilder();
+        $qb->leftJoin('d.favorites', 'u');
+        $qb->andWhere('u = :user');
+        $qb->setParameter('user', $user);
+        $qb->orderBy('d.dateCreation', 'DESC');
+        return $this->getPaginator($qb->getQuery());
+    }
 
-	public function findDecklistsInHallOfFame()
-	{
-		$qb = $this->getQueryBuilder();
-		$qb->andWhere('d.nbVotes > 10');
-		$qb->orderBy('d.nbVotes', 'DESC');
-		return $this->getPaginator($qb->getQuery());
-	}
+    public function findDecklistsByAuthor (User $user)
+    {
+        $qb = $this->getQueryBuilder();
+        $qb->andWhere('d.user = :user');
+        $qb->setParameter('user', $user);
+        $qb->orderBy('d.dateCreation', 'DESC');
+        return $this->getPaginator($qb->getQuery());
+    }
 
-	public function findDecklistsInHotTopic()
-	{
-		$qb = $this->getQueryBuilder();
-		$qb->addSelect('(SELECT count(c) FROM AppBundle:Comment c WHERE c.decklist=d AND DATE_DIFF(CURRENT_TIMESTAMP(), c.dateCreation)<1) AS HIDDEN nbRecentComments');
-		$qb->orderBy('nbRecentComments', 'DESC');
-		$qb->orderBy('d.nbComments', 'DESC');
-		return $this->getPaginator($qb->getQuery());
-	}
+    public function findDecklistsInHallOfFame ()
+    {
+        $qb = $this->getQueryBuilder();
+        $qb->andWhere('d.nbVotes > 10');
+        $qb->orderBy('d.nbVotes', 'DESC');
+        return $this->getPaginator($qb->getQuery());
+    }
 
-	public function findDecklistsInTournaments()
-	{
-		$qb = $this->getQueryBuilder();
-		$qb->andWhere('d.tournament is not null');
-		$qb->orderBy('d.dateCreation', 'DESC');
-		return $this->getPaginator($qb->getQuery());
-	}
+    public function findDecklistsInHotTopic ()
+    {
+        $qb = $this->getQueryBuilder();
+        $qb->addSelect('(SELECT count(c) FROM AppBundle:Comment c WHERE c.decklist=d AND DATE_DIFF(CURRENT_TIMESTAMP(), c.dateCreation)<1) AS HIDDEN nbRecentComments');
+        $qb->orderBy('nbRecentComments', 'DESC');
+        $qb->orderBy('d.nbComments', 'DESC');
+        return $this->getPaginator($qb->getQuery());
+    }
 
-	public function findDecklistsWithComplexSearch()
-	{
-		$request = $this->request_stack->getCurrentRequest();
+    public function findDecklistsInTournaments ()
+    {
+        $qb = $this->getQueryBuilder();
+        $qb->andWhere('d.tournament is not null');
+        $qb->orderBy('d.dateCreation', 'DESC');
+        return $this->getPaginator($qb->getQuery());
+    }
 
-		$cards_code = $request->query->get('cards');
-		if(!is_array($cards_code)) {
-			$cards_code = [];
-		}
+    public function findDecklistsWithComplexSearch ()
+    {
+        $request = $this->request_stack->getCurrentRequest();
 
-		$faction_code = filter_var($request->query->get('faction'), FILTER_SANITIZE_STRING);
-		if($faction_code) {
-			$faction = $this->doctrine->getRepository('AppBundle:Faction')->findOneBy(['code' => $faction_code]);
-		}
+        $cards_code = $request->query->get('cards');
+        if(!is_array($cards_code)) {
+            $cards_code = [];
+        }
 
-		$author_name = filter_var($request->query->get('author'), FILTER_SANITIZE_STRING);
+        $faction_code = filter_var($request->query->get('faction'), FILTER_SANITIZE_STRING);
+        if($faction_code) {
+            $faction = $this->doctrine->getRepository('AppBundle:Faction')->findOneBy(['code' => $faction_code]);
+        }
 
-		$decklist_name = filter_var($request->query->get('name'), FILTER_SANITIZE_STRING);
+        $author_name = filter_var($request->query->get('author'), FILTER_SANITIZE_STRING);
 
-		$sort = $request->query->get('sort');
+        $decklist_name = filter_var($request->query->get('name'), FILTER_SANITIZE_STRING);
 
-		$packs = $request->query->get('packs');
+        $sort = $request->query->get('sort');
 
-		$qb = $this->getQueryBuilder();
-		$joinTables = [];
-		
-		if(!empty($faction)) {
-			$qb->andWhere('d.faction = :faction');
-			$qb->setParameter('faction', $faction);
-		}
-		if(!empty($author_name)) {
-			$qb->innerJoin('d.user', 'u');
-			$joinTables[] = 'd.user';
-			$qb->andWhere('u.username = :username');
-			$qb->setParameter('username', $author_name);
-		}
-		if(! empty($decklist_name)) {
-			$qb->andWhere('d.name like :deckname');
-			$qb->setParameter('deckname', "%$decklist_name%");
-		}
-		if(!empty($cards_code) || !empty($packs)) {
-			if (!empty($cards_code) ) {
-				foreach ($cards_code as $i => $card_code) {
-					/* @var $card \AppBundle\Entity\Card */
-					$card = $this->doctrine->getRepository('AppBundle:Card')->findOneBy(array('code' => $card_code));
-					if(!$card) continue;
+        $packs = $request->query->get('packs');
 
-					$qb->innerJoin('d.slots', "s$i");
-					$qb->andWhere("s$i.card = :card$i");
-					$qb->setParameter("card$i", $card);
+        $qb = $this->getQueryBuilder();
+        $joinTables = [];
 
-					$packs[] = $card->getPack()->getId();
-				}
-			}
-			if (!empty($packs)) {
-				$sub = $this->doctrine->createQueryBuilder();
-				$sub->select("c");
-				$sub->from("AppBundle:Card","c");
-				$sub->innerJoin('AppBundle:Decklistslot', 's', 'WITH', 's.card = c');
-				$sub->where('s.decklist = d');
-				$sub->andWhere($sub->expr()->notIn('c.pack', $packs));
+        if(!empty($faction)) {
+            $qb->andWhere('d.faction = :faction');
+            $qb->setParameter('faction', $faction);
+        }
+        if(!empty($author_name)) {
+            $qb->innerJoin('d.user', 'u');
+            $joinTables[] = 'd.user';
+            $qb->andWhere('u.username = :username');
+            $qb->setParameter('username', $author_name);
+        }
+        if(!empty($decklist_name)) {
+            $qb->andWhere('d.name like :deckname');
+            $qb->setParameter('deckname', "%$decklist_name%");
+        }
+        if(!empty($cards_code) || !empty($packs)) {
+            if(!empty($cards_code)) {
+                foreach($cards_code as $i => $card_code) {
+                    /* @var $card \AppBundle\Entity\Card */
+                    $card = $this->doctrine->getRepository('AppBundle:Card')->findOneBy(array('code' => $card_code));
+                    if(!$card)
+                        continue;
 
-				$qb->andWhere($qb->expr()->not($qb->expr()->exists($sub->getDQL())));
-			}
-		}
+                    $qb->innerJoin('d.slots', "s$i");
+                    $qb->andWhere("s$i.card = :card$i");
+                    $qb->setParameter("card$i", $card);
 
-		switch($sort) {
-			case 'date':
-				$qb->orderBy('d.dateCreation', 'DESC');
-				break;
-			case 'likes':
-				$qb->orderBy('d.nbVotes', 'DESC');
-				break;
-			case 'reputation':
-				if(!in_array('d.user', $joinTables)) {
-					$qb->innerJoin('d.user', 'u');
-				}
-				$qb->orderBy('u.reputation', 'DESC');
-				break;
-			case 'popularity':
-			default:
-				$qb->addSelect('(1+d.nbVotes)/(1+POWER(DATE_DIFF(CURRENT_TIMESTAMP(), d.dateCreation), 2)) AS HIDDEN popularity');
-				$qb->orderBy('popularity', 'DESC');
-				break;
-		}
-		
-		return $this->getPaginator($qb->getQuery());
-	}
+                    if(!empty($packs)) {
+                        $packs[] = $card->getPack()->getId();
+                    }
+                }
+            }
+            if(!empty($packs)) {
+                $sub = $this->doctrine->createQueryBuilder();
+                $sub->select("c");
+                $sub->from("AppBundle:Card", "c");
+                $sub->innerJoin('AppBundle:Decklistslot', 's', 'WITH', 's.card = c');
+                $sub->where('s.decklist = d');
+                $sub->andWhere($sub->expr()->notIn('c.pack', $packs));
 
-	public function getNumberOfPages()
-	{
-		return intval(ceil($this->maxcount / $this->limit));
-	}
+                $qb->andWhere($qb->expr()->not($qb->expr()->exists($sub->getDQL())));
+            }
+        }
 
-	public function getAllPages()
-	{
-		$request = $this->request_stack->getCurrentRequest();
-		$route = $request->get('_route');
-		$route_params = $request->get('_route_params');
-		$query = $request->query->all();
+        switch($sort) {
+            case 'date':
+                $qb->orderBy('d.dateCreation', 'DESC');
+                break;
+            case 'likes':
+                $qb->orderBy('d.nbVotes', 'DESC');
+                break;
+            case 'reputation':
+                if(!in_array('d.user', $joinTables)) {
+                    $qb->innerJoin('d.user', 'u');
+                }
+                $qb->orderBy('u.reputation', 'DESC');
+                break;
+            case 'popularity':
+            default:
+                $qb->addSelect('(1+d.nbVotes)/(1+POWER(DATE_DIFF(CURRENT_TIMESTAMP(), d.dateCreation), 2)) AS HIDDEN popularity');
+                $qb->orderBy('popularity', 'DESC');
+                break;
+        }
 
-		$params = $query + $route_params;
+        return $this->getPaginator($qb->getQuery());
+    }
 
-		$number_of_pages = $this->getNumberOfPages();
-		$pages = [];
-		for ($page = 1; $page <= $number_of_pages; $page ++) {
-			$pages[] = array(
-				"numero" => $page,
-				"url" => $this->router->generate($route, ["page" => $page] + $params),
-				"current" => $page == $this->page
-			);
-		}
-		return $pages;
-	}
+    public function getNumberOfPages ()
+    {
+        return intval(ceil($this->maxcount / $this->limit));
+    }
 
-	public function getClosePages()
-	{
-		$allPages = $this->getAllPages();
-		$numero_courant = $this->page - 1;
-		$pages = [];
-		foreach($allPages as $numero => $page) {
-			if($numero === 0
-					|| $numero === count($allPages) - 1
-					|| abs($numero - $numero_courant) <= 2)
-			{
-				$pages[] = $page;
-			}
-		}
-		return $pages;
-	}
+    public function getAllPages ()
+    {
+        $request = $this->request_stack->getCurrentRequest();
+        $route = $request->get('_route');
+        $route_params = $request->get('_route_params');
+        $query = $request->query->all();
 
-	public function getPreviousUrl()
-	{
-		if($this->page === 1) return null;
+        $params = $query + $route_params;
 
-		$request = $this->request_stack->getCurrentRequest();
-		$route = $request->get('_route');
-		$routeParams = $request->get('_route_params');
-		$params = $request->query->all();
-		$previous_page = max(1, $this->page - 1);
+        $number_of_pages = $this->getNumberOfPages();
+        $pages = [];
+        for($page = 1; $page <= $number_of_pages; $page ++) {
+            $pages[] = array(
+                "numero" => $page,
+                "url" => $this->router->generate($route, ["page" => $page] + $params),
+                "current" => $page == $this->page
+            );
+        }
+        return $pages;
+    }
 
-		return $this->router->generate($route, [ "page" => $previous_page ] + $routeParams + $params);
-	}
+    public function getClosePages ()
+    {
+        $allPages = $this->getAllPages();
+        $numero_courant = $this->page - 1;
+        $pages = [];
+        foreach($allPages as $numero => $page) {
+            if($numero === 0 || $numero === count($allPages) - 1 || abs($numero - $numero_courant) <= 2) {
+                $pages[] = $page;
+            }
+        }
+        return $pages;
+    }
 
-	public function getNextUrl()
-	{
-		if($this->page === $this->getNumberOfPages()) return null;
+    public function getPreviousUrl ()
+    {
+        if($this->page === 1)
+            return null;
 
-		$request = $this->request_stack->getCurrentRequest();
-		$route = $request->get('_route');
-		$routeParams = $request->get('_route_params');
-		$params = $request->query->all();
-		$next_page = min($this->getNumberOfPages(), $this->page + 1);
+        $request = $this->request_stack->getCurrentRequest();
+        $route = $request->get('_route');
+        $routeParams = $request->get('_route_params');
+        $params = $request->query->all();
+        $previous_page = max(1, $this->page - 1);
 
-		return $this->router->generate($route, [ "page" => $next_page ] + $routeParams + $params);
-	}
+        return $this->router->generate($route, ["page" => $previous_page] + $routeParams + $params);
+    }
+
+    public function getNextUrl ()
+    {
+        if($this->page === $this->getNumberOfPages())
+            return null;
+
+        $request = $this->request_stack->getCurrentRequest();
+        $route = $request->get('_route');
+        $routeParams = $request->get('_route_params');
+        $params = $request->query->all();
+        $next_page = min($this->getNumberOfPages(), $this->page + 1);
+
+        return $this->router->generate($route, ["page" => $next_page] + $routeParams + $params);
+    }
 
 }
