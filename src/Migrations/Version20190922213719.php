8e7c2fa51d304e4a5e95d7c729efb60f56b2f1c4
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-09-23 20:52:21 -0700

    back-fill uuids during migration.

--- src/Migrations/Version20190922213719.php
+++ src/Migrations/Version20190922213719.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Instantiator\Exception\ExceptionInterface;
 use Doctrine\Migrations\AbstractMigration;
+use Exception;
+use Ramsey\Uuid\Uuid;
 
 /**
  * Adds UUID column to deck table.
@@ -19,11 +22,20 @@ final class Version20190922213719 extends AbstractMigration
 
     /**
      * @inheritdoc
+     * @throws Exception
      */
     public function up(Schema $schema) : void
     {
         $this->addSql('ALTER TABLE deck ADD uuid CHAR(36) COMMENT \'(DC2Type:uuid)\'');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_4FAC3637D17F50A6 ON deck (uuid)');
+
+        $sql = 'SELECT id FROM deck ORDER BY id';
+        $rows = $this->connection->executeQuery($sql)->fetchAll();
+
+        foreach ($rows as $row) {
+            $uuid = Uuid::uuid4();
+            $this->addSql("UPDATE deck SET uuid = :uuid WHERE id = :id", ["uuid" => $uuid, "id" => $row["id"]]);
+        }
     }
 
     /**
