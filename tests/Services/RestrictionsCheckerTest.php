b1be072312630ad99f793d266b0e1186b2cddca4
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-12-27 17:21:23 -0800

    adds pods without restricted to restrictions checker.

--- tests/Services/RestrictionsCheckerTest.php
+++ tests/Services/RestrictionsCheckerTest.php
@@ -47,7 +47,15 @@ class RestrictionsCheckerTest extends TestCase
                         'title' => 'P2',
                         'restricted' => '01005',
                         'cards' => ['01003', '01004']
-                    ]
+                    ],
+                    [
+                        'title' => 'P3',
+                        'cards' => ['01006', '01007']
+                    ],
+                    [
+                        'title' => 'P4',
+                        'cards' => ['01008', '01009']
+                    ],
                 ]
             ],
             'melee' => [
@@ -62,15 +70,23 @@ class RestrictionsCheckerTest extends TestCase
                 ],
                 'restricted_pods' => [
                     [
-                        'title' => 'P1',
+                        'title' => 'MP1',
                         'restricted' => '01000',
                         'cards' => ['01001', '01002']
                     ],
                     [
-                        'title' => 'P2',
+                        'title' => 'MP2',
                         'restricted' => '01005',
                         'cards' => ['01003', '01004']
-                    ]
+                    ],
+                    [
+                        'title' => 'MP3',
+                        'cards' => ['01006', '01007']
+                    ],
+                    [
+                        'title' => 'MP4',
+                        'cards' => ['01008', '01009']
+                    ],
                 ]
             ],
         ];
@@ -90,6 +106,7 @@ class RestrictionsCheckerTest extends TestCase
             [['04001', '04002', '01000'], 'contains one restricted card'],
             [['01000', '01003', '01004'], 'contains one restricted card and cards from a different restricted-pod.'],
             [['01001', '01002'], 'contains cards in restricted-pod but not the restricted card itself.'],
+            [['01006', '01008'], 'contains cards in pods but only one from each pod max.'],
             [[], 'empty list']
         ];
     }
@@ -125,6 +142,7 @@ class RestrictionsCheckerTest extends TestCase
             [['04001', '04002', '01900'], 'contains banned card'],
             [['04001', '04002', '01000', '01005'], 'contains more than one restricted card'],
             [['01000', '01001'], 'contains restricted card and a card in its restricted-pod.'],
+            [['01006', '01007'], 'contains more than one card from a pod.'],
         ];
     }
 
